f846091db370af091469b148a55518a7f7f5a7df
Aaron Ritter <git@ritter.world>
2025-01-28 19:48:23 +0100

    build(deps): pump fusionauth-swift-sdk to 0.2.0 and fusionauth to 1.55.1

--- complete-application/QuickstartTests/QuickstartTests.swift
+++ complete-application/QuickstartTests/QuickstartTests.swift
@@ -31,7 +31,7 @@ final class QuickstartTests: XCTestCase {
         }
 
         let expectation = XCTNSPredicateExpectation(predicate: predicate, object: app)
-        wait(for: [expectation], timeout: 60)
+        wait(for: [expectation], timeout: 120)
         removeUIInterruptionMonitor(alertMonitor)
     }
 
@@ -47,9 +47,9 @@ final class QuickstartTests: XCTestCase {
         let passwordField = app.secureTextFields["Password"]
         let submitButton = app.buttons["Submit"]
 
-        XCTAssertTrue(emailField.waitForExistence(timeout: 30))
-        XCTAssertTrue(passwordField.waitForExistence(timeout: 30))
-        XCTAssertTrue(submitButton.waitForExistence(timeout: 30))
+        XCTAssertTrue(emailField.waitForExistence(timeout: 60))
+        XCTAssertTrue(passwordField.waitForExistence(timeout: 60))
+        XCTAssertTrue(submitButton.waitForExistence(timeout: 60))
 
         emailField.tap()
         emailField.typeText("richard@example.com")
@@ -62,7 +62,7 @@ final class QuickstartTests: XCTestCase {
         // Check that Welcome message is displayed
         let welcomeText = app.staticTexts["Welcome Richard Hendricks"]
 
-        XCTAssertTrue(welcomeText.waitForExistence(timeout: 30))
+        XCTAssertTrue(welcomeText.waitForExistence(timeout: 60))
 
         // Check that Log out button is displayed
         let logoutButton = app.buttons["Log out"]
@@ -71,6 +71,6 @@ final class QuickstartTests: XCTestCase {
 
         confirmLoginAlert(app)
 
-        XCTAssertTrue(loginButton.waitForExistence(timeout: 30))
+        XCTAssertTrue(loginButton.waitForExistence(timeout: 60))
     }
 }
